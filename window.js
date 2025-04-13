512e35606123f883d670674f31e28cf9326fa2c2
Araceli Gonzalez Luna <araceliluna.0999@gmail.com>
2025-04-12 21:28:42 -0600

    modificaciones

--- window.js
+++ window.js
@@ -1,4 +1,4 @@
-//Encontrar la palabra mas larga
+//Encontrar la palabra mas larga de un array
 function palabraMasLarga(arr) {
     let inicio = 0;
     let maxLength = 0;
@@ -24,6 +24,31 @@ const array = [
 ];
 
 console.log(palabraMasLarga(array));
+
+//Encontrar palabra mas larga en un parrafo de texto
+
+function palabraMasLarga(parrafo) {
+    const palabras = parrafo.split(' ');
+
+    let maxLength = 0;
+    let palabra = "";
+
+    for (let fin = 0; fin < palabras.length; fin++) {
+        const actualPalabra = palabras.slice(fin, fin + 1)[0];
+
+        if (actualPalabra.length > maxLength) {
+            maxLength = actualPalabra.length;
+            palabra = actualPalabra;
+        }
+    }
+
+    return { palabra, maxLength };
+}
+
+const parrafo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
+console.log(palabraMasLarga(parrafo));
+
 
 
 
